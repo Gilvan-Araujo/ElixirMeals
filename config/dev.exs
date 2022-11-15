3c94bc728964dadec7a9948211d01a6d4cca2959
Gilvan Araujo <gilvanjunior@protonmail.com>
2022-11-15 06:17:21 -0300

    project config and early tests

--- config/dev.exs
+++ config/dev.exs
@@ -4,8 +4,8 @@ use Mix.Config
 config :exmeal, Exmeal.Repo,
   username: "postgres",
   password: "postgres",
-  database: "exmeal_dev",
   hostname: "localhost",
+  database: "exmeal_dev",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
