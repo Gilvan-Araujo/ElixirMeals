fb2fb121b43f04787cfec9ecb15a8849b7b2e07f
Gilvan Araujo <gilvanjunior@protonmail.com>
2022-11-16 06:50:49 -0300

    feat: create files and test for create use case

--- priv/repo/migrations/20210415171125_create_meals_table.exs
+++ priv/repo/migrations/20210415171125_create_meals_table.exs
@@ -2,6 +2,14 @@ defmodule Exmeal.Repo.Migrations.CreateMealsTable do
   use Ecto.Migration
 
   def change do
-   # TO DO
+    create table(:meals) do
+      add(:description, :string)
+      add(:date, :date)
+      add(:calories, :integer)
+
+      timestamps()
+    end
+
+    create(unique_index(:meals, [:id]))
   end
 end
